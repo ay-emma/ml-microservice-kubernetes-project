b25d2e9e4dd4d6f4c9de68097768363357916dbe
Ubuntu <ubuntu@ip-172-31-72-69.ec2.internal>
2022-07-26 10:14:21 +0000

    ready for circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,9 +8,10 @@ dockerpath=whoisay/ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-# minikube kubectl run --image=$dockerpath ml-app
 
-minikube kubectl -- create deployment ml-app --image=$dockerpath
+minikube kubectl run ml-app --image=$dockerpath 
+
+# minikube kubectl -- create deployment ml-app --image=$dockerpath
 
 
 # Step 3:
@@ -19,9 +20,14 @@ minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-minikube kubectl port-forward ml-app 8000:80
+# minikube kubectl port-forward ml-app-58cb888466-bm26w 8000:80
 
-# minikube kubectl -- expose deployment ml-app --type=NodePort --port=8000
+# minikube kubectl port-forward ml-app-58cb888466-5dhxp 8000:80
+minikube kubectl port-forward ml-app 8080:80
+
+# minikube kubectl -- expose deployment ml-app --type=NodePort --port=80
+# minikube delete --all
+
 
 
 
